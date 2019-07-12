1d8cc9d521cb3f39ba92d1525d4e698c06c6afab
sagar <sagar@amt.in>
2019-07-12 13:13:27 +0530

    data-binding basics

--- basics-of-angular/src/app/app.module.ts
+++ basics-of-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './firstcomponent/first.component';
@@ -12,7 +13,8 @@ import { SecondComponent } from './secondcomponent/second.component';
     SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- basics-of-angular/src/app/secondcomponent/second.component.ts
+++ basics-of-angular/src/app/secondcomponent/second.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 
 export class SecondComponent {
     title = 'This is my second component'
+    inputvalue = '';
+
+
+    constructor() {
+        setTimeout(()=>{
+            this.title = 'checking property binding'
+        }, 2000)
+    }
+
+    clickevent(event: Event){
+    //    console.log(event);
+    this.inputvalue = (<HTMLInputElement> event.target).value; //currect way of doing this 
+    // this.inputvalue = event.target.value;
+
+     console.log(this.inputvalue);
+    }
 }
